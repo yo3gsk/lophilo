20dc4195af9157484d7df0ce2ce8a8c834dc4644
Ricky Ng-Adam <rngadam@lophilo.com>
2012-03-08 22:14:20 +0800

    clang test and a script connecting to all hosts

--- masquerade/common.sh
+++ masquerade/common.sh
@@ -4,10 +4,12 @@ export CCACHE_DIR=/gdata/ccachevol
 export CCACHE_SLOPPINESS="time_macros file_macro include_file_mtime"
 export PREFIX=/usr/bin/
 
-export CXX="ccache ${PREFIX}g++"
+#export CXX="ccache ${PREFIX}g++"
+export CXX="ccache ${PREFIX}clang -Qunused-arguments"
 export CXXFLAGS="$PLATFORM_FLAGS" 
 export CFLAGS="$CXXFLAGS"
 export CPPFLAGS="$CXXFLAGS"
-export CC="ccache ${PREFIX}gcc"
+#export CC="ccache ${PREFIX}gcc"
+export CC="ccache ${PREFIX}clang -mcpu=i686 -Qunused-arguments"
 export LD=${PREFIX}ld
 export STRIP=${PREFIX}strip
