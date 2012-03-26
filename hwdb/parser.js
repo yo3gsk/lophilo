4357d10ac1a8b2d30666af2d0b03944fd6239910
Ricky Ng-Adam <rngadam@lophilo.com>
2012-03-26 15:20:30 +0800

    with apricot

--- hwdb/parser.js
+++ hwdb/parser.js
@@ -1,28 +1,13 @@
 #!/usr/bin/env node
 
-var libxmljs = require('libxmljs');
+var Apricot = require('apricot');
 var fs = require('fs');
 
-// xpath queries
-//var gchild = xmlDoc.get('//grandchild');
-
-//console.log(gchild.text());  // prints 'grandchild content'
-
-//var children = xmlDoc.root().childNodes();
-//var child = children[0];
-
-//console.log(child.attr('foo').value()); // prints 'bar'
-
-
-function parseFileContent(err, content) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('read: ' + content.length);
-  var xmldoc = libxmljs.parseXmlString(content);
-  console.dir('xmldoc:' + xmldoc);
+function processHtmlDoc(error, htmldoc) {
+	console.log('starting: ');
+  console.dir(htmldoc);
   r = require('repl').start();
-  r.context.xmldoc = xmldoc;
+  r.context.htmldoc = htmldoc;
 }
 
-fs.readFile('./library_report.xml', 'utf-8', parseFileContent);
+Apricot.open('library_report.doc', processHtmlDoc);
